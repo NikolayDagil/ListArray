2cd33bc7d774b91e3b57794d045c4132bfbd746b
Nikolay Dagil <Nikolay.Dagil@gmail.com>
2012-10-13 17:08:30 +0300

    Filled the entire project

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Ввод, вывод данных, замена, удаление 
+
+		/** ArrayList<String> list = new ArrayList<String>();
+
+		// System.out.println(list.size());
+
+		 list.add("A");
+		 list.add("B");
+		 list.add("C");
+		 list.add("A");
+		 list.add("E");
+
+		 System.out.println(list.size());
+		 System.out.println(list);
+
+		 list.remove(0);
+		 System.out.print("Размер массива ");
+		 System.out.println(list.size());
+		 System.out.print("Вывести элемент ");
+		 System.out.println(list.get(3));
+		 System.out.print("Результат ");
+		 System.out.println(list);
+		 System.out.print("Заменить элемент ");
+		 System.out.println(list.set(0, "D"));
+		 System.out.print("Вывод ");
+		 System.out.println(list);*/
+		
+		// работа с массивами
+
+		/**ArrayList<Integer> mas = new ArrayList<Integer>();
+		mas.add(1);
+		mas.add(2);
+		mas.add(3);
+		mas.add(4);
+		mas.add(5);
+		System.out.print("Результат: ");
+		System.out.println(mas);
+
+		Integer[] massive = new Integer[mas.size()];
+		massive = mas.toArray(massive);
+
+		int sum = 0;
+
+		for (int i : massive)
+
+			sum += i;
+
+		System.out.print("Сумма = " + sum);*/
+
+	}
+
+}
